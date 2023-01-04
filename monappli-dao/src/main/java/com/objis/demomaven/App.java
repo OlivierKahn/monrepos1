b3f80ff2f07694b15aa3596f5e9dc2f4449ff3fe
OlivierKahn <121880721+OlivierKahn@users.noreply.github.com>
2023-01-04 16:34:11 +0100

    correct

--- monappli-dao/src/main/java/com/objis/demomaven/App.java
+++ monappli-dao/src/main/java/com/objis/demomaven/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.p
-            rintln( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
